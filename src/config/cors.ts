df701ad4cf245497840cea9c113fea36132da2d1
AJ Derteano <andresderteano@outlook.com>
2025-05-19 22:21:44 -0500

    fix: update CORS configuration to specify allowed origins and enhance Docker container management

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,7 +1,14 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 export const CORS: CorsOptions = {
-  origin: ['*'],
+  origin: [
+    'http://127.0.0.1:5173',
+    'http://localhost:5173',
+    'https://ajderteano.nom.pe',
+    'http://ajderteano.nom.pe',
+    'https://www.ajderteano.nom.pe',
+    'http://www.ajderteano.nom.pe',
+  ],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   credentials: true,
 };
